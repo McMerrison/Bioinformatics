3944aba5aa971ea19b6e82b0e28746a593ba21f0
U-MCMERRISON\Talha <talhehtasham@gmail.com>
2016-09-18 17:39:22 -0400

    HW1

--- HW1.pl
+++ HW1.pl
@@ -1,10 +1,25 @@
-=
+=begin
 Name: Talha Ehtasham
 CISC 436 Bioinformatics
 Homework 1
 =cut
 
+$length = 500;
+
 sub rand_seq
+{
+	my @chars = ("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V");
+	my $seq;
+	for ($a = 0; $a < $length; $a++) {
+		$seq .= $chars[rand @chars];
+	}
+	$seq;
+}
+
+sub mutate_seq
 {
 	
 }
+
+$sequence = &rand_seq;
+printf "$sequence";
