cc9650eaf279835b09f7937e25005d90dc31a453
U-MCMERRISON\Talha <talhehtasham@gmail.com>
2016-09-20 17:11:06 -0400

    Read pam1 file

--- HW1.pl
+++ HW1.pl
@@ -4,13 +4,13 @@ CISC 436 Bioinformatics
 Homework 1
 =cut
 
-use strict;
+#use strict;
 use warnings;
 
 my $length = 500;
-my $matrix;
+my @matrix;
 
-$matrix[0][0] = 9867; $matrix[0][1] = 2; $matrix[0][2] = 9; $matrix[0][3] = 10;
+
 
 sub rand_seq
 {
@@ -25,23 +25,33 @@ sub rand_seq
 sub mutate_seq
 {
 	my $str = $_[0];
-	for (my $i = 0; $i < $length; $i++) {
+	for (my $p = 0; $p < $length; $p++) {
 		my $evolve = &randcalc;
 		
-		my $test = substr($str, $i, 1);
+		my $test = substr($str, $p, 1);
 	}
 }
 
 sub readpam
 {
+	my $i = 0;
+	my $j = 0;
 	my $filename = 'pam1.txt';
 	open(my $pam, $filename) or die "ERROR";
 	
 	while (my $row = <$pam>) {
-		my $chars = substr($row, 0, 5);
-		#chomp $row;
-		print "$chars\n";
-		sleep(1);
+		my @tokens = split /\s{1,}/, $row;
+		foreach my $token(@tokens) {
+			
+			$matrix[$i][$j] = $token;
+			#print "$token ";
+			$j++;
+		}
+		print "\n";
+		#print "$j";
+		$i++;
+		$j = 0;
+		
 	}
 }
 sub randcalc
@@ -50,6 +60,17 @@ sub randcalc
 	$random;
 }
 
+sub printmatrix
+{
+	for($row = 0; $row < 20; $row++) {
+		for($col = 0; $col < 20; $col++) {
+			print "$matrix[$row][$col] ";
+		}
+		print "\n";
+	}
+}
+
 my $sequence = &rand_seq;
 &readpam;
+&printmatrix;
 &mutate_seq($sequence);
