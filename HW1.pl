8256efbd4a0f6aff8ef8f0ca80eefc60422b7d74
Talha E <Talha E>
2016-09-20 02:40:18 +0100

    HW1

--- HW1.pl
+++ HW1.pl
@@ -4,7 +4,13 @@ CISC 436 Bioinformatics
 Homework 1
 =cut
 
-$length = 500;
+use strict;
+use warnings;
+
+my $length = 500;
+my $matrix;
+
+$matrix[0][0] = 9867; $matrix[0][1] = 2; $matrix[0][2] = 9; $matrix[0][3] = 10;
 
 sub rand_seq
 {
@@ -18,8 +24,32 @@ sub rand_seq
 
 sub mutate_seq
 {
-	
+	my $str = $_[0];
+	for (my $i = 0; $i < $length; $i++) {
+		my $evolve = &randcalc;
+		
+		my $test = substr($str, $i, 1);
+	}
 }
 
-$sequence = &rand_seq;
-printf "$sequence";
+sub readpam
+{
+	my $filename = 'pam1.txt';
+	open(my $pam, $filename) or die "ERROR";
+	
+	while (my $row = <$pam>) {
+		my $chars = substr($row, 0, 5);
+		#chomp $row;
+		print "$chars\n";
+		sleep(1);
+	}
+}
+sub randcalc
+{
+	my $random = rand()*10000;
+	$random;
+}
+
+my $sequence = &rand_seq;
+&readpam;
+&mutate_seq($sequence);
